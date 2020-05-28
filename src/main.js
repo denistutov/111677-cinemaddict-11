84c550c22d4d78e7c0b0fd05d00412076eee1338
Denis Tutov <vrednokot@gmail.com>
2020-05-28 09:39:21 +0300

    рефакторинг main

--- src/main.js
+++ src/main.js
@@ -15,45 +15,51 @@ const pageHeader = document.querySelector(`.header`);
 const pageMain = document.querySelector(`.main`);
 const footerStatistic = document.querySelector(`.footer__statistics`);
 
+const moviesModel = new Movies();
+const api = new API(AUTHORIZATION);
+
+const filterController = new FilterController(pageMain, moviesModel);
 const pageLoadingComponent = new PageLoading();
 const filmsCardsComponent = new FilmListContainer();
 const sortFilmsComponent = new FilmsSort();
+
+filterController.render();
 render(pageMain, sortFilmsComponent, RenderPosition.BEFOREEND);
 render(pageMain, pageLoadingComponent, RenderPosition.BEFOREEND);
 
-const moviesModel = new Movies();
-const api = new API(AUTHORIZATION);
+const afterDataLoad = (filmCards) => {
+  const statisticComponent = new Statistic(moviesModel);
+  const pageHeaderComponent = new UserRank(moviesModel);
+
+  const filmStatisticsComponent = new FilmStatistics(moviesModel.getFilmCardsAll());
+  const renderCardsBoard = new CardsBoardController(filmsCardsComponent, sortFilmsComponent, moviesModel, api);
+
+  renderCardsBoard.render(filmCards);
+  filterController.render();
+  render(pageHeader, pageHeaderComponent, RenderPosition.BEFOREEND);
+  render(pageMain, filmsCardsComponent, RenderPosition.BEFOREEND);
+  render(pageMain, statisticComponent, RenderPosition.BEFOREEND);
+  render(footerStatistic, filmStatisticsComponent, RenderPosition.BEFOREEND);
+
+  filterController.addStatisticsButtonClickHandler(() => {
+    renderCardsBoard.hide();
+    sortFilmsComponent.hide();
+    statisticComponent.show();
+    statisticComponent.render();
+  });
+
+  filterController.addFilterChangeHandler(() => {
+    statisticComponent.hide();
+    renderCardsBoard.show();
+    sortFilmsComponent.show();
+  });
+
+  statisticComponent.hide();
+  remove(pageLoadingComponent);
+};
 
 api.getFilmCards()
   .then((filmCards) => {
     moviesModel.setFilmCards(filmCards);
-
-    const statisticComponent = new Statistic(moviesModel);
-    const pageHeaderComponent = new UserRank(moviesModel);
-    const filterController = new FilterController(pageMain, moviesModel);
-    const filmStatisticsComponent = new FilmStatistics(moviesModel.getFilmCardsAll());
-    const renderCardsBoard = new CardsBoardController(filmsCardsComponent, sortFilmsComponent, moviesModel, api);
-
-    renderCardsBoard.render(filmCards);
-    filterController.render();
-    render(pageHeader, pageHeaderComponent, RenderPosition.BEFOREEND);
-    render(pageMain, filmsCardsComponent, RenderPosition.BEFOREEND);
-    render(pageMain, statisticComponent, RenderPosition.BEFOREEND);
-    render(footerStatistic, filmStatisticsComponent, RenderPosition.BEFOREEND);
-
-    filterController.addStatisticsButtonClickHandler(() => {
-      renderCardsBoard.hide();
-      sortFilmsComponent.hide();
-      statisticComponent.show();
-      statisticComponent.render();
-    });
-
-    filterController.addFilterChangeHandler(() => {
-      statisticComponent.hide();
-      renderCardsBoard.show();
-      sortFilmsComponent.show();
-    });
-
-    statisticComponent.hide();
-    remove(pageLoadingComponent);
+    afterDataLoad(filmCards);
   });
